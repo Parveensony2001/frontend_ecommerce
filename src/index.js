fd85051e66a6ebc620fb28a3c0e9a4cd87b22a61
Praveen soni <parveensony2001@gmail.com>
2023-09-20 16:01:43 +0530

    k

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Parveensony2001/Node_js_Important.git
+git push -u origin master
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
